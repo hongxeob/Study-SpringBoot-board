5787a571b815ed957e5288ecaf15d5fa8bb4c48c
hongxeob <sjun2918@naver.com>
2022-10-23 17:38:44 +0900

    페이징 처리2(게시물 페이지 선택 추가)

--- Desktop/workspace_itj/board/src/main/java/com/study/board/controller/BoardController.java
+++ Desktop/workspace_itj/board/src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -25,18 +26,29 @@ public class BoardController {
     }
 
     @PostMapping("/board/writedo")
-    public String boardWriteDo(Board board , Model model, MultipartFile file) throws Exception{
-        boardService.write(board,file);
-        model.addAttribute("message","글 작성이 완료 되었습니다.");
-        model.addAttribute("searchUrl","/board/list");
+    public String boardWriteDo(Board board, Model model, MultipartFile file) throws Exception {
+        boardService.write(board, file);
+        model.addAttribute("message", "글 작성이 완료 되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
 
         return "message";
     }
 
     @GetMapping("/board/list")
-    public String boardList(Model model,@PageableDefault(page = 0,size = 10,direction = Sort.Direction.DESC,sort = "id") Pageable pageable) {
+    private String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 
-        model.addAttribute("list", boardService.boardList(pageable));
+        Page<Board> list = boardService.boardList(pageable);
+
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage - 4, 1);
+        int endPage = Math.min(nowPage + 5, list.getTotalPages());
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         return "boardlist";
     }
 
@@ -60,13 +72,13 @@ public class BoardController {
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board,Model model,MultipartFile file) throws Exception {
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model, MultipartFile file) throws Exception {
 
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
         boardService.write(boardTemp, file);
-        model.addAttribute("message","글 수정이 완료 되었습니다.");
+        model.addAttribute("message", "글 수정이 완료 되었습니다.");
         model.addAttribute("searchUrl", "/board/list");
         return "message";
     }
